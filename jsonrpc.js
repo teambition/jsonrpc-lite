b8cd6a9da9deaf83a2dac62748c017f2793538f7
zensh <admin@zensh.com>
2015-04-25 09:54:10 +0800

    fix and improve

--- jsonrpc.js
+++ jsonrpc.js
@@ -13,226 +13,239 @@
 }(typeof window === 'object' ? window : this, function() {
   'use strict';
 
-  var slice = Array.prototype.slice;
   var toString = Object.prototype.toString;
   var hasOwnProperty = Object.prototype.hasOwnProperty;
   var isArray = Array.isArray || function(obj) {
     return toString.call(obj) === '[object Array]';
   };
 
-  function isString(obj) {
-    return obj && typeof obj === 'string';
-  }
+  var jsonrpc = {
+    JsonRpc: JsonRpc,
+    JsonRpcError: JsonRpcError
+  };
 
-  function isInteger(obj) {
-    return typeof obj === 'number' && (obj % 1 === 0);
-  }
+  /**
+   * Creates a JSON-RPC 2.0 request object
+   *
+   * @param  {String|Integer} id
+   * @param  {String} method
+   * @param  {Object|Array} [params]: optional
+   * @return {Object} JsonRpc object
+   * @api public
+   */
+  jsonrpc.request = function(id, method, params) {
+    var err = validateMessage('request', {id: id, method: method, params: params});
+    if (err) throw err;
+    return requestObject(id, method, params);
+  };
 
-  function isObject(obj) {
-    return obj && typeof obj === 'object' && !isArray(obj);
-  }
+  /**
+   * Creates a JSON-RPC 2.0 notification object
+   *
+   * @param  {String} method
+   * @param  {Object|Array} [params]: optional
+   * @return {Object} JsonRpc object
+   * @api public
+   */
+  jsonrpc.notification = function(method, params) {
+    var err = validateMessage('notification', {method: method, params: params});
+    if (err) throw err;
+    return notificationObject(method, params);
+  };
 
-  function isNull(obj) {
-    return obj == null;
-  }
+  /**
+   * Creates a JSON-RPC 2.0 success response object
+   *
+   * @param  {String|Integer} id
+   * @param  {Mixed} result
+   * @return {Object} JsonRpc object
+   * @api public
+   */
+  jsonrpc.success = function(id, result) {
+    var err = validateMessage('success', {id: id, result: result});
+    if (err) throw err;
+    return successObject(id, result);
+  };
 
-  function inherits(Child, Parent) {
-    function Ctor() {
-      this.constructor = Child;
+  /**
+   * Creates a JSON-RPC 2.0 error response object
+   *
+   * @param  {String|Integer} id
+   * @param  {Object} JsonRpcError error
+   * @return {Object} JsonRpc object
+   * @api public
+   */
+  jsonrpc.error = function(id, error) {
+    var err = validateMessage('error', {id: id, error: error});
+    if (err) throw err;
+    return errorObject(id, error);
+  };
+
+  /**
+   * Takes a JSON-RPC 2.0 payload (string) and tries to parse it into a JSON.
+   * If successful, determine what object is it (response, notification,
+   * success, error, or invalid), and return it's type and properly formatted object.
+   *
+   * @param  {String} msg
+   * @return {Object|Array} an array, or an object of this format:
+   *
+   *  {
+   *    type: <Enum, 'request'|'notification'|'success'|'error'|'invalid'>
+   *    payload: <JsonRpc|JsonRpcError>
+   *  }
+   *
+   * @api public
+   */
+  jsonrpc.parse = function (message) {
+    try {
+      message = JSON.parse(message);
+    } catch (err) {
+      throw JsonRpcError.parseError(err);
     }
+    if (!isArray(message)) return parseObject(message);
+    for (var i = 0, len = message.length; i < len; i++)
+      message[i] = parseObject(message[i]);
 
-    Ctor.prototype = Parent.prototype;
-    Child.prototype = new Ctor();
-    Child.__super__ = Parent.prototype;
-    return Child;
-  }
+    return message;
+  };
 
-  var jsonrpc = {JsonRpc: JsonRpc};
-
+  /**
+   * JsonRpc Class
+   *
+   * @return {Object} JsonRpc object
+   * @api public
+   */
   function JsonRpc() {
     this.jsonrpc = '2.0';
   }
+
   JsonRpc.VERSION = '2.0';
   JsonRpc.prototype.serialize = JsonRpc.prototype.toString = function() {
     return JSON.stringify(this);
   };
 
   /**
-   * Creates a JSON-RPC 2.0 serialized
+   * JsonRpcError Class
    *
-   * @param  {String} id
-   * @param  {String} method
-   * @param  {Object|Array} params
-   * @return {Object} JsonRpc object
+   * @param  {String} message
+   * @param  {Integer} code: optional
+   * @return {String} name: optional
+   * @api public
    */
-   jsonrpc.request = function(id, method, params) {
-    validateMessage('request', {id: id, method: method, params: params});
+  function JsonRpcError(message, code, data) {
+    this.code = code;
+    this.message = message;
+    if (data != null) this.data = data;
+  }
 
+  inherits(JsonRpcError, Error);
+  JsonRpcError.prototype.name = 'JsonRpcError';
+
+  JsonRpcError.invalidRequest = function(data) {
+    return new JsonRpcError('Invalid request', -32600, data);
+  };
+
+  JsonRpcError.methodNotFound = function(data) {
+    return new JsonRpcError('Method not found', -32601, data);
+  };
+
+  JsonRpcError.invalidParams = function(data) {
+    return new JsonRpcError('Invalid params', -32602, data);
+  };
+
+  JsonRpcError.internalError = function(data) {
+    return new JsonRpcError('Internal error', -32603, data);
+  };
+
+  JsonRpcError.parseError = function(data) {
+    return new JsonRpcError('Parse error', -32700, data);
+  };
+
+  /**
+   * @api private
+   */
+  function requestObject(id, method, params) {
     var message = new JsonRpc();
     message.id = id;
     message.method = method;
     if (params) message.params = params;
     return message;
-  };
+  }
 
   /**
-   * Creates a JSON-RPC 2.0 serialized notification:
-   *
-   * @param  {String} method
-   * @param  {Object|Array} params
-   * @return {Object} JsonRpc object
+   * @api private
    */
-  jsonrpc.notification = function(method, params) {
-    validateMessage('notification', {method: method, params: params});
-
+  function notificationObject(method, params) {
     var message = new JsonRpc();
     message.method = method;
     if (params) message.params = params;
     return message;
-  };
+  }
 
   /**
-   * Creates a JSON-RPC 2.0 serialized success response
-   *
-   * @param  {String} id
-   * @param  {Mixed} result
-   * @return {Object} JsonRpc object
+   * @api private
    */
-  jsonrpc.success = function(id, result) {
-    validateMessage('success', {id: id, result: result});
-
+  function successObject(id, result) {
     var message = new JsonRpc();
     message.id = id;
     message.result = result;
     return message;
-  };
+  }
 
   /**
-   * Creates a JSON-RPC 2.0 serialized error response
-   *
-   * @param  {String} id
-   * @param  {Mixed} error
-   * @return {Object} JsonRpc object
+   * @api private
    */
-  jsonrpc.error = function(id, error) {
-    validateMessage('error', {id: id, error: error});
-
+  function errorObject(id, error) {
     var message = new JsonRpc();
     message.id = id;
-    message.error = error.toJSON;
+    message.error = error;
     return message;
-  };
+  }
 
   /**
-   * Takes a JSON-RPC 2.0 payload (string) and tries to parse it into a JSON.
-   * If successful, determine what object is it (response, notification,
-   * success, or invalid), and return its type and properly formatted object.
-   *
-   * @param  {String} msg
-   * @return {Object} an object of this format:
-   *
-   *  {
-   *    type: <Enum, request|notification|success|error>
-   *    payload: <Object>
-   *  }
+   * @api private
    */
-  jsonrpc.parse = function(message) {
-    try {
-      message = JSON.parse(message);
-    } catch (err) {
-      throw ParseError(err);
-    }
-    if (!isArray(message)) return parseMessage(message);
-    for (var i = 0, len = message.length; i < len; i++)
-      message[i] = parseMessage(message[i]);
+  function parseObject(obj) {
+    var error = null;
+    var res = {type: 'invalid'};
 
-    return message;
-  };
+    if (obj.jsonrpc !== JsonRpc.VERSION) {
+      error = JsonRpcError.internalError();
 
+    } else if (!hasOwnProperty.call(obj, 'id')) {
+      error = validateMessage('notification', obj);
+      if (!error) {
+        res.type = 'notification';
+        res.payload = notificationObject(obj.method, obj.params);
+      }
+    } else if (hasOwnProperty.call(obj, 'method')) {
+      error = validateMessage('request', obj);
+      if (!error) {
+        res.type = 'request';
+        res.payload = requestObject(obj.id, obj.method, obj.params);
+      }
+    } else if (hasOwnProperty.call(obj, 'result')) {
+      error = validateMessage('success', obj);
+      if (!error) {
+        res.type = 'success';
+        res.payload = successObject(obj.id, obj.result);
+      }
+    } else if (hasOwnProperty.call(obj, 'error')) {
+      if (!obj.error) {
+        error = JsonRpcError.internalError();
+      } else {
+        obj.error = new JsonRpcError(obj.error.message, obj.error.code, obj.error.data);
+        error = validateMessage('error', obj);
+      }
 
-  function parseMessage(message) {
-    var error;
-    var res = {type: 'invalid', payload: message};
+      if (!error) {
+        res.type = 'error';
+        res.payload = errorObject(obj.id, obj.error);
+      }
+    } else error = JsonRpcError.internalError();
 
-    if (message.jsonrpc !== JsonRpc.VERSION) {
-      error = new InternalError();
-
-    } else if (!hasOwnProperty.call(message, 'id')) {
-      error = validateMessage('notification', message);
-      if (!error) res.type = 'notification';
-
-    } else if (hasOwnProperty.call(message, 'method')) {
-      error = validateMessage('request', message);
-      if (!error) res.type = 'request';
-
-    } else if (hasOwnProperty.call(message, 'result')) {
-      error = validateMessage('success', message);
-      if (!error) res.type = 'success';
-
-    } else if (hasOwnProperty.call(message, 'error')) {
-      error = validateMessage('error', message);
-      if (!error) res.type = 'error';
-
-    } else error = new InternalError();
-
-    if (!error) res.payload = error;
+    if (error) res.payload = error;
     return res;
-  }
-
-  inherits(JsonRpcError, Error);
-  inherits(ParseError, JsonRpcError);
-  inherits(InternalError, JsonRpcError);
-  inherits(InvalidParamsError, JsonRpcError);
-  inherits(InvalidRequestError, JsonRpcError);
-  inherits(MethodNotFoundError, JsonRpcError);
-
-  jsonrpc.err = {
-    ParseError: ParseError,
-    JsonRpcError: JsonRpcError,
-    InternalError: InternalError,
-    InvalidParamsError: InvalidParamsError,
-    InvalidRequestError: InvalidRequestError,
-    MethodNotFoundError: MethodNotFoundError
-  };
-
-  function JsonRpcError(message, code) {
-    this.name = 'JsonRpcError';
-    this.code = code;
-    this.message = message;
-  }
-
-  JsonRpcError.prototype.toJSON = function() {
-    return {code: this.code, message: this.message};
-  };
-
-  function ParseError(message) {
-    this.name = 'ParseError';
-    this.code = -32700;
-    this.message = message || 'Invalid JSON was received by the server.';
-  }
-
-  function InternalError(message) {
-    this.name = 'InternalError';
-    this.code = -32603;
-    this.message = message || 'Internal JSON-RPC error';
-  }
-
-  function InvalidParamsError(message) {
-    this.name = 'InvalidParamsError';
-    this.code = -32602;
-    this.message = message || 'Invalid method parameter(s)';
-  }
-
-  function InvalidRequestError(message) {
-    this.name = 'InvalidRequestError';
-    this.code = -32600;
-    this.message = message || 'The JSON sent is not a valid Request object.';
-  }
-
-  function MethodNotFoundError(message) {
-    this.name = 'MethodNotFoundError';
-    this.code = -32601;
-    this.message = message || 'The method does not exist / is not available.';
   }
 
   // if error, return error, else return null
@@ -250,16 +263,20 @@
       case 'error':
         return checkId(data.id) || checkError(data.error);
     }
-    return new InternalError();
+    return JsonRpcError.internalError();
   }
 
   function checkId(id) {
-    var check = id === null || isString(id) || isInteger(id);
-    return check === true ? null : new InternalError('"id" must be provided. It must be either a string or an integer.');
+    return (isNull(id) || isString(id) || isInteger(id)) ? null :
+      JsonRpcError.internalError('"id" must be provided. It must be either a string or an integer.');
   }
 
   function checkMethod(method) {
-    return isString(method) ? null : new MethodNotFoundError();
+    return isString(method) ? null : JsonRpcError.methodNotFound();
+  }
+
+  function checkResult(result) {
+    return result === void 0 ? JsonRpcError.internalError('Result must exist for success Response objects') : null;
   }
 
   function checkParams(params) {
@@ -270,29 +287,50 @@
         JSON.stringify(params);
         return null;
       } catch (e) {
-        return new ParseError(e.message);
+        return JsonRpcError.parseError(e.message);
       }
     }
-    return new InvalidParamsError();
-  }
-
-  function checkResult(result) {
-    return isNull(result) ? new InternalError('Result must exist for success Response objects') : null;
+    return JsonRpcError.invalidParams();
   }
 
   function checkError(error) {
     if (!(error instanceof JsonRpcError))
-      return new JsonRpcError('Error must be an instance of JsonRpcError, or any derivatives of it');
+      return JsonRpcError.internalError('Error must be an instance of JsonRpcError.');
 
     if (!isInteger(error.code))
-      return new JsonRpcError('Invalid error code. It must be an integer.');
+      return JsonRpcError.internalError('Invalid error code. It must be an integer.');
 
     if (!isString(error.message))
-      return new JsonRpcError('Message must exist or must be a string.');
+      return JsonRpcError.internalError('Message must exist or must be a string.');
 
     return null;
   }
 
-  return jsonrpc;
+  function isNull(obj) {
+    return obj === null;
+  }
 
+  function isString(obj) {
+    return obj && typeof obj === 'string';
+  }
+
+  function isInteger(obj) {
+    return typeof obj === 'number' && (obj % 1 === 0);
+  }
+
+  function isObject(obj) {
+    return obj && typeof obj === 'object' && !isArray(obj);
+  }
+
+  function inherits(Child, Parent) {
+    function Ctor() {
+      this.constructor = Child;
+    }
+
+    Ctor.prototype = Parent.prototype;
+    Child.prototype = new Ctor();
+    return Child;
+  }
+
+  return jsonrpc;
 }));
